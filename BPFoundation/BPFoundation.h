b496a9666eb0a8db4543e8f010f849c9f5774595
Brian Partridge <brianpartridge@gmail.com>
2012-11-21 15:34:05 -0500

    add skeleton project

---
+++ BPFoundation/BPFoundation.h
@@ -0,0 +1,13 @@
+//
+//  BPFoundation.h
+//  BPFoundation
+//
+//  Created by Brian Partridge on 11/21/12.
+//  Copyright (c) 2012 Brian Partridge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BPFoundation : NSObject
+
+@end
